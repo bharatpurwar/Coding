ab2d9cde956433112fbebc900a9604b3a940f943
Bharat Purwar <bharat.purwar@globallogic.com>
2022-05-10 09:27:39 +0530

    first commmit

---
+++ NewFile.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class NewFile {
+    
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        String firstname = input.next();
+
+        System.out.println(firstname);
+    } 
+}
